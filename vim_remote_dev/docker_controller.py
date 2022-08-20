91e66ad889676cbc6f421d76c86caae62893e4d0
Sanskar Jethi <sansyrox@gmail.com>
2022-08-20 02:15:10 +0100

    incremental updates

--- vim_remote_dev/docker_controller.py
+++ vim_remote_dev/docker_controller.py
@@ -1,10 +1,7 @@
 import subprocess
 
 async def return_config_setup_command(config_directory_path):
-    if config_directory_path.startswith("http"):
-        return f"git clone {config_directory_path}"
-    else:
-        return f"cp -R {config_directory_path} ."
+    return f"git clone {config_directory_path} nvim".split()
 
 def process_env_variables_files(file_path):
     lines = []
@@ -15,16 +12,21 @@ def process_env_variables_files(file_path):
 
 async def setup_environment(service_name, config_directory_path, env_variables_file, editor_install_command, language_server_install_command, editor_choice, plugin_manager_install_command):
     subprocess.call(["docker-compose", "build"])
-    subprocess.call(["docker-compose", "run", service_name, await return_config_setup_command(config_directory_path)])
-    # export commands here
-    subprocess.call(["docker-compose", "run", service_name, f"bash {env_variables_file}"])
+    subprocess.call(["docker-compose", "run", service_name, *await return_config_setup_command(config_directory_path)])
 
-    subprocess.call(["docker-compose", "run", service_name, editor_install_command])
-    subprocess.call(["docker-compose", "run", service_name, language_server_install_command])
+    # initialise the base directory here
+    print("Making the directory")
+    subprocess.call(["docker-compose", "run", service_name, "mkdir", "-p", "~/.config/nvim"])
+
+    print("Moving the files")
+    subprocess.call(["docker-compose", "run", service_name, "mv", "nvim", "~/.config/nvim"])
+
+    # export commands here
+    subprocess.call(["docker-compose", "run", service_name, *( f"sh -c '{env_variables_file}'".split() )])
+
+    subprocess.call(["docker-compose", "run", service_name, "sh", "-c", editor_install_command])
+    subprocess.call(["docker-compose", "run", service_name, *( language_server_install_command.split() )])
 
     subprocess.call(["docker-compose", "run", service_name, f"{editor_choice} -c '{plugin_manager_install_command}' -c 'q'"])
-
-
-
 
 
